c99efaa324b708214a7c8bdbff2627e09b5f3f7b
Patrick762 <lutzpatti86@gmail.com>
2025-12-25 15:58:48 +0100

    update ep500 fields

--- bluetti_bt_lib/devices/ep500.py
+++ bluetti_bt_lib/devices/ep500.py
@@ -8,8 +8,8 @@ class EP500(BaseDeviceV1):
         super().__init__(
             [
                 EnumField(FieldName.AC_OUTPUT_MODE, 70, OutputMode),
-                DecimalField(FieldName.INTERNAL_AC_VOLTAGE, 71, 1, 10),
-                DecimalField(FieldName.INTERNAL_AC_FREQUENCY, 74, 2, 10),
+                DecimalField(FieldName.AC_OUTPUT_VOLTAGE, 71, 1),
+                DecimalField(FieldName.AC_OUTPUT_FREQUENCY, 74, 2),
                 DecimalField(FieldName.AC_INPUT_VOLTAGE, 77, 1),
                 DecimalField(FieldName.AC_INPUT_FREQUENCY, 80, 2),
                 UIntField(FieldName.PV_S1_VOLTAGE, 86, 0.1),
