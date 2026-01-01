a11721da9036cb034565bb9034f4fb04c43db9e1
Erik - Selfmadestrom <info@selfmadestrom.de>
2026-01-01 12:46:11 +0100

    Add EP2000 device

--- bluetti_bt_lib/devices/__init__.py
+++ bluetti_bt_lib/devices/__init__.py
@@ -22,6 +22,7 @@ from .ep500 import EP500
 from .ep500p import EP500P
 from .ep600 import EP600
 from .ep800 import EP800
+from .ep2000 import EP2000
 from .handsfree1 import Handsfree1
 
 # Add new device classes here
@@ -47,11 +48,12 @@ DEVICES = {
     "EP500": EP500,
     "EP500P": EP500P,
     "EP600": EP600,
+    "EP2000": EP2000,
     "EP800": EP800,
     "Handsfree 1": Handsfree1,
 }
 
 # Prefixes of all currently supported devices
 DEVICE_NAME_RE = re.compile(
-    r"^(AC2A|AC50B|AC60|AC60P|AC70|AC70P|AC180|AC180T|AC180P|AC200L|AC200M|AC200PL|AC300|AC500|AP300|EB3A|EL100V2|EL30V2|EP500|EP500P|EP600|EP760|EP800|Handsfree\s1)(\d+)$"
+    r"^(AC2A|AC50B|AC60|AC60P|AC70|AC70P|AC180|AC180T|AC180P|AC200L|AC200M|AC200PL|AC300|AC500|AP300|EB3A|EL100V2|EL30V2|EP500|EP500P|EP600|EP2000|EP760|EP800|Handsfree\s1)(\d+)$"
 )
